dd84b84fe2e07c852a53c7416abbcb68c1459d0c
Rakesh Kumar <rakeshpersonal@gmail.com>
2017-01-25 11:05:44 +0530

    update footer and page title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ class Footer extends Component {
             <div className="todo-footer">
                 <hr/>
                 <p>Double-click to edit a todo!</p>
+                <p>Used <a href="https://facebook.github.io/react/" target="_blank">React(a javascript library for building UI)</a> and ES6</p>
                 <p>Created by <strong>
                     <a href="https://github.com/rakeshongithub/Todo-App-ReactJs"
                        target="_blank">Rakesh Kumar</a>
